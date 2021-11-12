54b4c92a6cc5e85f147d31e72ac2be17b8a3b16c
Victor Miene <viktormiene.vm@gmail.com>
2021-11-12 20:13:31 +0100

    Feat: trying out more functions from the singleton

--- index.js
+++ index.js
@@ -33,3 +33,47 @@ const addSongOfIce = BookServiceSingleton.addBook(
 const taleOfTwoCities = BookServiceSingleton.addBook(
   new Book("0003", "A Tale of Two Cities", " Charles Dickens", "1995")
 );
+
+//Added so output can be easily seen
+console.log("---------------------------");
+console.log("Available books", BookServiceSingleton.books);
+console.log("---------------------------");
+const isRemoved = BookServiceSingleton.removeBook("0001");
+console.log("remove gatsby: ", isRemoved);
+//Added so output can be easily seen
+console.log("---------------------------");
+console.log("Available books", BookServiceSingleton.books);
+console.log("---------------------------");
+console.log(
+  "is Gatsby Available:",
+  BookServiceSingleton.isBookAvailable("0001")
+);
+
+console.log("unregister jon: ", BookServiceSingleton.unregisterUser(jon));
+console.log("---------------------------");
+console.log("All users", Object.keys(BookServiceSingleton.users));
+console.log("---------------------------");
+console.log("Before lending\n");
+console.log("Douyeszn lend:", BookServiceSingleton.hasUserLend("douyeszn"));
+console.log("Deen lend:", BookServiceSingleton.hasUserLend("deen"));
+
+console.log(
+  "Users and Books Collected (initially empty):",
+  BookServiceSingleton.usersAndBooksCollected
+);
+console.log("---------------------------");
+console.log("Users lend books\n");
+console.log(
+  "douyeszn lends A tale of two cities",
+  BookServiceSingleton.lendBook("douyeszn", "0003")
+);
+console.log(
+  "douyeszn lends Song of Ice and Fire",
+  BookServiceSingleton.lendBook("douyeszn", "0002")
+);
+console.log("deen lends Song of Ice and Fire", deen.lendBook("0002"));
+console.log("Douyeszn lend:", BookServiceSingleton.hasUserLend("douyeszn"));
+console.log(
+  "Users and Books Collected (After users lend)\n",
+  BookServiceSingleton.usersAndBooksCollected
+);
